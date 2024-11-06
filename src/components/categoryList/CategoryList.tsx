05ddafb5d1e9122ae6145792f5f81de7b5e45d0d
Jerry Melton <jerryleemelton@gmail.com>
2024-11-06 10:06:57 -0800

    Put in rough structure for category list

--- src/components/categoryList/CategoryList.tsx
+++ src/components/categoryList/CategoryList.tsx
@@ -1,8 +1,22 @@
 import React from "react"
+import Image from "next/image"
 import styles from "./CategoryList.module.css"
+import Link from "next/link"
 
 const CategoryList: React.FC = () => {
-  return <div className={styles.container}>CategoryList</div>
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Categories</h1>
+      <div className={styles.categories}>
+        <div className={styles.category}>
+          <Link href="/blog?cat=coding">
+            <Image src="/coding.png" alt="" width={25} height={25} />
+            Cat. Name
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CategoryList
