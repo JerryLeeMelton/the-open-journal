52766bd1487ce6634431e4ab2407d2e36b437349
Jerry Melton <jerryleemelton@gmail.com>
2024-11-07 09:23:49 -0800

    Add base for category buttons

--- src/components/categoryList/CategoryList.tsx
+++ src/components/categoryList/CategoryList.tsx
@@ -8,12 +8,84 @@ const CategoryList: React.FC = () => {
     <div className={styles.container}>
       <h1 className={styles.title}>Categories</h1>
       <div className={styles.categories}>
-        <div className={styles.category}>
-          <Link href="/blog?cat=coding">
-            <Image src="/coding.png" alt="" width={25} height={25} />
-            Cat. Name
-          </Link>
-        </div>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.coding}`}
+        >
+          <Image
+            src="/style.png"
+            alt=""
+            className={styles.image}
+            width={32}
+            height={32}
+          />
+          Coding
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.music}`}
+        >
+          <Image
+            src="/style.png"
+            alt=""
+            className={styles.image}
+            width={32}
+            height={32}
+          />
+          Music
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.film}`}
+        >
+          <Image
+            src="/style.png"
+            alt=""
+            className={styles.image}
+            width={32}
+            height={32}
+          />
+          Film
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.food}`}
+        >
+          <Image
+            src="/style.png"
+            alt=""
+            className={styles.image}
+            width={32}
+            height={32}
+          />
+          Food
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.mtg}`}
+        >
+          <Image
+            src="/style.png"
+            alt=""
+            className={styles.image}
+            width={32}
+            height={32}
+          />
+          MTG
+        </Link>
+        <Link
+          href="/blog?cat=style"
+          className={`${styles.category} ${styles.philosophy}`}
+        >
+          <Image
+            src="/style.png"
+            alt=""
+            className={styles.image}
+            width={32}
+            height={32}
+          />
+          Philosophy
+        </Link>
       </div>
     </div>
   )
