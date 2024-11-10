c4cff76daa1094778d52b0b5d6418f731b66255f
Jerry Melton <jerryleemelton@gmail.com>
2024-11-09 16:48:55 -0800

    Style changes to category and featured section

--- src/components/categoryList/CategoryList.tsx
+++ src/components/categoryList/CategoryList.tsx
@@ -4,88 +4,34 @@ import styles from "./CategoryList.module.css"
 import Link from "next/link"
 
 const CategoryList: React.FC = () => {
+  const categories = [
+    { name: "Coding", slug: "coding" },
+    { name: "Music", slug: "music" },
+    { name: "Film", slug: "film" },
+    { name: "Food", slug: "food" },
+    { name: "MTG", slug: "mtg" },
+    { name: "Philosophy", slug: "philosophy" },
+  ]
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Categories</h1>
       <div className={styles.categories}>
-        <Link
-          href="/blog?cat=style"
-          className={`${styles.category} ${styles.coding}`}
-        >
-          <Image
-            src="/style.png"
-            alt=""
-            className={styles.image}
-            width={32}
-            height={32}
-          />
-          Coding
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          className={`${styles.category} ${styles.music}`}
-        >
-          <Image
-            src="/style.png"
-            alt=""
-            className={styles.image}
-            width={32}
-            height={32}
-          />
-          Music
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          className={`${styles.category} ${styles.film}`}
-        >
-          <Image
-            src="/style.png"
-            alt=""
-            className={styles.image}
-            width={32}
-            height={32}
-          />
-          Film
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          className={`${styles.category} ${styles.food}`}
-        >
-          <Image
-            src="/style.png"
-            alt=""
-            className={styles.image}
-            width={32}
-            height={32}
-          />
-          Food
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          className={`${styles.category} ${styles.mtg}`}
-        >
-          <Image
-            src="/style.png"
-            alt=""
-            className={styles.image}
-            width={32}
-            height={32}
-          />
-          MTG
-        </Link>
-        <Link
-          href="/blog?cat=style"
-          className={`${styles.category} ${styles.philosophy}`}
-        >
-          <Image
-            src="/style.png"
-            alt=""
-            className={styles.image}
-            width={32}
-            height={32}
-          />
-          Philosophy
-        </Link>
+        {categories.map((category) => (
+          <Link
+            href={`/blog?cat=${category.slug}`}
+            className={`${styles.category} ${styles[category.slug]}`}
+          >
+            <Image
+              src={`/${category.slug}.png`}
+              alt=""
+              className={styles.image}
+              width={32}
+              height={32}
+            />
+            {category.name}
+          </Link>
+        ))}
       </div>
     </div>
   )
