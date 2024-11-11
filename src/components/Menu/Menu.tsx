4e68e0144a88fb29acbcaf773f72977b898917c7
Jerry Melton <jerryleemelton@gmail.com>
2024-11-11 11:07:27 -0800

    Styling for side menu categories

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -8,6 +8,7 @@ import SideMenuCategories from "../sideMenuCategories/SideMenuCategories"
 const Menu: React.FC = () => {
   return (
     <div className={styles.container}>
+      {/* Popular Posts section */}
       <h2 className={styles.sideMenuSubtitle}>What's Hot?</h2>
       <h1 className={styles.sideMenuTitle}>Popular Posts</h1>
       <SideMenuPosts withImage={false} />
@@ -15,52 +16,9 @@ const Menu: React.FC = () => {
       {/* Category section */}
       <SideMenuCategories />
 
-      <div className={styles.categoryContainer}>
-        <h2 className={styles.sideMenuSubtitle}>Discover by Topic</h2>
-        <h1 className={styles.sideMenuTitle}>Categories</h1>
-        <div className={styles.categoryList}>
-          <Link
-            href="/blog?cat=coding"
-            className={`${styles.categoryItem} ${styles.coding}`}
-          >
-            Coding
-          </Link>
-          <Link
-            href="/blog?cat=music"
-            className={`${styles.categoryItem} ${styles.music}`}
-          >
-            Music
-          </Link>
-          <Link
-            href="/blog?cat=film"
-            className={`${styles.categoryItem} ${styles.film}`}
-          >
-            Film
-          </Link>
-          <Link
-            href="/blog?cat=gaming"
-            className={`${styles.categoryItem} ${styles.gaming}`}
-          >
-            Gaming
-          </Link>
-          <Link
-            href="/blog?cat=food"
-            className={`${styles.categoryItem} ${styles.food}`}
-          >
-            Food
-          </Link>
-          <Link
-            href="/blog?cat=philosophy"
-            className={`${styles.categoryItem} ${styles.philosophy}`}
-          >
-            Philosophy
-          </Link>
-        </div>
-      </div>
-      {/* 2nd section */}
+      {/* Author's Picks section */}
       <h2 className={styles.sideMenuSubtitle}>Chosen by the author</h2>
       <h1 className={styles.sideMenuTitle}>Author's Picks</h1>
-
       <SideMenuPosts withImage={true} />
     </div>
   )
