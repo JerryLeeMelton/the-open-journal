e441a7eec900b6e9e5b9e3d42e1d2fc85fc0e0ed
Jerry Melton <jerryleemelton@gmail.com>
2024-11-11 13:26:11 -0800

    Implement pagination buttons

--- src/components/pagination/Pagination.tsx
+++ src/components/pagination/Pagination.tsx
@@ -2,7 +2,12 @@ import React from "react"
 import styles from "./Pagination.module.css"
 
 const Pagination: React.FC = () => {
-  return <div className={styles.container}>Pagination</div>
+  return (
+    <div className={styles.container}>
+      <button className={styles.button}>Previous</button>
+      <button className={styles.button}>Next</button>
+    </div>
+  )
 }
 
 export default Pagination
