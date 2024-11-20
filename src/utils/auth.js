fe209730ef0cc6f93a45f4bd90f2a88538e5475e
Jerry Melton <jerryleemelton@gmail.com>
2024-11-20 14:15:10 -0800

    Setup base for API and utils

---
+++ src/utils/auth.js
@@ -0,0 +1,12 @@
+export const authOptions = {
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+}
